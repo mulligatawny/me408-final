8cdf4799a5300737c33507e60d8233882819b92d
mark benjamin <47930526+mulligatawny@users.noreply.github.com>
2021-03-18 16:42:44 -0700

    Delete subroutines directory

--- subroutines/compute_vorticity.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def compute_vorticity(N, n1, n2, uk, vk):
-    dvdx = np.fft.ifftn(np.fft.ifftshift(1j*n1*vk))*N**2
-    dudy = np.fft.ifftn(np.fft.ifftshift(1j*n2*uk))*N**2
-    return np.real(dvdx - dudy)
