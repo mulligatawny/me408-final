00ca93b6bc5d2ea9e5b628ea2c15ad8edb2b93ec
mulligatawny <mail.markbenjamin@gmail.com>
2021-03-19 09:14:44 -0700

    add N32 figs

---
+++ navier-stokes/data/32x32/t0pt5/ns.py
@@ -0,0 +1,115 @@
+###############################################################################
+# 2D Incompressible Navier-Stokes Solver with Periodic Boundaries, Galerkins' #
+# Method and Adams-Bashforth Time Integration                                 #
+###############################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+from subroutines import compute_fk
+from subroutines import compute_vorticity
+from subroutines import compute_pressure
+from matplotlib import cm
+import pdb
+
+def fu(uk, vk):
+    fku = compute_fk.compute_fk(N, n1, n2, uk, vk, dim=0)
+    fkv = compute_fk.compute_fk(N, n1, n2, uk, vk, dim=1)
+    a = np.zeros_like(uk)
+    d = np.zeros_like(uk)
+    for i in range(N):
+        for j in range(N):
+            # singular k**2
+            if n1[i] == n2[j] == 0:
+                a[i,j] = 0
+                d[i,j] = 0
+            else:
+                a[i,j] = (n1[i]*fku[i,j] + n2[j]*fkv[i,j])*\
+                n1[i]/(n1[i]**2 + n2[j]**2)
+                d[i,j] = nu*(n1[i]**2 + n2[j]**2)*uk[i,j]
+    return fku -a -d
+
+def fv(uk, vk):
+    fku = compute_fk.compute_fk(N, n1, n2, uk, vk, dim=0)
+    fkv = compute_fk.compute_fk(N, n1, n2, uk, vk, dim=1)
+    a = np.zeros_like(vk)
+    d = np.zeros_like(vk)
+    for i in range(N):
+        for j in range(N):
+            if n1[i] == n2[j] == 0:
+                a[i,j] = 0
+                d[i,j] = 0
+            else:
+                a[i,j] = (n1[i]*fku[i,j] + n2[j]*fkv[i,j]\
+                            )*n2[j]/(n1[i]**2 + n2[j]**2)
+                d[i,j] = nu*(n1[i]**2 + n2[j]**2)*vk[i,j]
+    return fkv -a -d
+
+N = 32
+L = 2*np.pi
+nu = 1
+
+# mesh
+x = np.linspace(-L/2, L/2, N+1)[:-1]
+y = np.linspace(-L/2, L/2, N+1)[:-1]
+X, Y = np.meshgrid(x, y)
+
+# wavenumbers
+n1 = np.arange(-N/2, N/2)*(2*np.pi/L)
+n2 = np.arange(-N/2, N/2)*(2*np.pi/L)
+
+# initialize
+u0 = 0.5*(np.sin(Y+X) +np.sin(Y-X))
+v0 = -0.5*(np.sin(X+Y) +np.sin(X-Y))
+t = 0.0
+tf = 0.5
+dt = 5e-4
+nt = int(tf/dt+1)
+e = np.zeros(nt)
+count = 0
+
+# transform I.C.
+uk = np.fft.fftshift(np.fft.fft2(u0))/N**2
+vk = np.fft.fftshift(np.fft.fft2(v0))/N**2
+
+# allocate storage for (n+1) and (n-1)th timestep
+uknp1 = np.zeros_like(uk)
+vknp1 = np.zeros_like(vk)
+uknm1 = np.zeros_like(uk)
+vknm1 = np.zeros_like(vk)
+
+# first timestep with forward Euler
+uknp1 = uk + dt*fu(uk, vk)
+vknp1 = vk + dt*fv(uk, vk)
+uknm1 = uk
+vknm1 = vk
+uk = uknp1
+vk = vknp1
+t = t + dt
+
+# time integrate using Adams-Bashforth
+while t < tf:
+    uknp1 = uk + (dt/2)*(3*fu(uk, vk) - fu(uknm1, vknm1))
+    vknp1 = vk + (dt/2)*(3*fv(uk, vk) - fv(uknm1, vknm1))
+    uknm1 = uk
+    vknm1 = vk
+    uk = uknp1
+    vk = vknp1
+    u = np.real(np.fft.ifft2(np.fft.ifftshift(uk))*(N**2))
+    v = np.real(np.fft.ifft2(np.fft.ifftshift(vk))*(N**2))
+    e[count] = (np.mean(u**2) + np.mean(v**2))/2
+    count = count + 1
+    t = t + dt
+
+u = np.real(np.fft.ifft2(np.fft.ifftshift(uk))*(N**2))
+v = np.real(np.fft.ifft2(np.fft.ifftshift(vk))*(N**2))
+w = compute_vorticity.compute_vorticity(N, n1, n2, uk, vk)
+p = compute_pressure.compute_pressure(N, n1, n2, uk, vk)
+
+np.save('t.npy', t)
+np.save('x.npy', X)
+np.save('y.npy', Y)
+np.save('u.npy', u)
+np.save('v.npy', v)
+np.save('w.npy', w)
+np.save('p.npy', p)
+np.save('e.npy', e)
--- navier-stokes/data/32x32/t2/post.py
+++ navier-stokes/data/32x32/t2/post.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from subroutines import compute_fk
 from matplotlib import cm
 
 u = np.load('./u.npy')
@@ -11,13 +12,38 @@ w = np.load('./w.npy')
 p = np.load('./p.npy')
 e = np.load('./e.npy')
 
-plt.contourf(x, y, u, cmap=cm.inferno, vmin=-.1, vmax=.1)
-#plt.contourf(x, y, w, cmap=cm.binary)
+fig1 = plt.figure(1)
+plt.contourf(x, y, u, cmap=cm.inferno, vmin=-.05, vmax=.05)
+plt.colorbar()
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.title('Contours of $u$ at t = {:.2f}'.format(t))
+plt.quiver(x, y, u, v, cmap=cm.gray)
+plt.show()
+
+fig2 = plt.figure(2)
+plt.contourf(x, y, v, cmap=cm.inferno, vmin=-.05, vmax=.05)
+plt.colorbar()
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.title('Contours of $v$ at t = {:.2f}'.format(t))
+plt.quiver(x, y, u, v, cmap=cm.gray)
+plt.show()
+
+fig3 = plt.figure(3)
+plt.contourf(x, y, w, cmap=cm.GnBu, vmin=-.05, vmax=.05)
 plt.colorbar()
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.title('Contours of $\omega$ at t = {:.2f}'.format(t))
 plt.quiver(x, y, u, v, cmap=cm.gray)
+plt.show()
 
-#plt.semilogy((e[:-1]))
+fig4 = plt.figure(4)
+plt.contourf(x, y, p, cmap=cm.OrRd, vmin=.9998, vmax=1.0002)
+plt.colorbar()
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.title('Contours of $p$ at t = {:.2f}'.format(t))
+plt.quiver(x, y, u, v, cmap=cm.gray)
 plt.show()
